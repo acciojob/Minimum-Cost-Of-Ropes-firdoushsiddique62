1c7d21a98f82ea1416ba313a8c34f19274be2000
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-19 18:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
- function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
- 
- 
- 
-function calculateMinCost() {
+ function calculateMinCost() {
   //your code here
-  let str=document.getElementById('rope-lengths').value.split(",");
-  let arr=str.map((str)=>parseInt(str));
-  // console.log(arr);
-  
-let total=0;
-arr=arr.sort(compare);
-// console.log("arr ",arr);
-while(arr.length>=2){
-  let sum=arr[0]+arr[1];
-  // console.log(sum);
-  let rem=[];
-  rem.push(sum);
-  for(let k=2;k<arr.length;k++){
-    rem.push(arr[k]);
+  let ans = 0,
+    arr = document
+      .getElementById("rope-lengths")
+      .value.split(",")
+      .map((val) => parseInt(val));
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    arr[1] += arr[0];
+    ans += arr[1];
+    arr.shift();
   }
-  rem=rem.sort(compare);
-  // console.log("rem ",rem);
-  arr=[...rem];
-  // console.log("arr ",arr);
-  total=total+sum;
-  // console.log("total "+total+"----------------------");
+  document.getElementById("result").innerText = ans;
 }
-console.log(total);
- 
- 
-let result=document.getElementById('result');
-result.innerHTML=total;
-}  
-  
